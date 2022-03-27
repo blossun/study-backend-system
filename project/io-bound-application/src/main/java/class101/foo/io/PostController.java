6901c965a06245ab5c0139e88da09380329e90dc
blossun <blossunn@gmail.com>
2022-03-28 00:18:36 +0900

    feat: 글 번호로 조회

--- project/io-bound-application/src/main/java/class101/foo/io/PostController.java
+++ project/io-bound-application/src/main/java/class101/foo/io/PostController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,7 +34,11 @@ public class PostController {
     }
 
     // 3. 글 번호로 조회
-    
+    @GetMapping("/post/{id}")
+    public Post getPostById(@PathVariable("id") Long id) {
+        return postRepository.findById(id).get();
+    }
+
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
 
 }
