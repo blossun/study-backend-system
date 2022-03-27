8db15917d89306f4e5b91ea51124352cc3eed90a
blossun <blossunn@gmail.com>
2022-03-28 00:13:23 +0900

    feat: paging 적용

--- project/io-bound-application/src/main/java/class101/foo/io/PostController.java
+++ project/io-bound-application/src/main/java/class101/foo/io/PostController.java
@@ -1,12 +1,19 @@
 package class101.foo.io;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
+
+    private static final Integer PAGE_SIZE = 20;
 
     @Autowired
     PostRepository postRepository;
@@ -17,14 +24,14 @@ public class PostController {
         return postRepository.save(post);
     }
 
-    // 2-1. 글 목록을 조회한다.
+    // 2. 글 목록을 페이징하여 반환
     @GetMapping("/posts")
-    public List<Post> getPostList() {
-        return postRepository.findAll();
+    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
+        return postRepository.findAll(
+                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+        );
     }
-    
-    // 2-2 글 목록을 페이징하여 반환
-    
+
     // 3. 글 번호로 조회
     
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
