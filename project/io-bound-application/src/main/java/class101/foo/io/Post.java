377573f394dbdc5701fdb6170dc908a1701d481a
blossun <blossunn@gmail.com>
2022-04-03 16:28:45 +0900

    feat: db에서 es를 사용하도록 변경

--- project/io-bound-application/src/main/java/class101/foo/io/Post.java
+++ project/io-bound-application/src/main/java/class101/foo/io/Post.java
@@ -1,15 +1,14 @@
 package class101.foo.io;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
 import lombok.Data;
 
-import javax.persistence.*;
-
 @Data
-@Entity(name = "post")
+@Document(indexName = "post")
 public class Post {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
-    @SequenceGenerator(name="post_id_seq", sequenceName = "post_id_seq", allocationSize = 1)
-    private Long id;
+    private String id;
     private String content;
 }
--- project/io-bound-application/src/main/java/class101/foo/io/PostRepository.java
+++ project/io-bound-application/src/main/java/class101/foo/io/PostRepository.java
@@ -2,11 +2,11 @@ package class101.foo.io;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends ElasticsearchRepository<Post, String> {
 
-    List<Post> findByContentContains(String content);
+    List<Post> findByContent(String content);
 }
